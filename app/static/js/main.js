b2c3d834f7f0e3e5fc0590a90ec080b8283ed6cf
jacob <15millerj@ellesmere.school.nz>
2020-10-22 14:19:17 +1300

    bit connnnnnnnnnnnnect

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -9,14 +9,59 @@ $(document).ready(function(){
 		$grid.masonry('layout');
 	});
 	
+	$('.grid-item').click(function(){
+		var image_data = $(this).data(image);
+		var image = image_data.image;
+		var description = `<p>${image.description}</p>`;
+		var title = `<h5 class="modal-title">${image.name}<i class="fa.fa-times" data-dismiss="modal" aria-label="Close" aria-hidden="true"></i></h5>`;
+		var img = `<img src="${image.upload_location}" alt="${image.name}">`;
+		$('#image-modal .modal-body').html(img + title + description);
+		$('.modal').modal('show');
+	});
+
+	$('i.like').click(function(e) {
+		
+		e.stopPropagation();
+		e.preventDefault();
+
+		var like = $(this).hasClass('far');
+		var image_id = $(this).data('image');
+		var _this = $(this);
+
+		$.getJSON(
+			$SCRIPT_ROOT + '/like',
+			{
+				like: like,
+				image_id: image_id
+			}, 
+			function(result) {
+				if (result) {
+					if (like) {
+						_this.removeClass('far');
+						_this.addClass('fas');
+					} else {
+						_this.removeClass('fas');
+						_this.addClass('far');
+					}
+				}
+			}
+		);
+		return false;
+	});
+	if ($('#filter-select').length > 0 ) {
+		var filter = $('#filter-select').data('filter');
+		$('#filter-select').val(filter);
+	}
+	
+	if ($('#category').length > 0 ) {
+		var category = $('#category').data('category');
+		$('#category').val(category);
+	}
+	
+	$('#filter-select').change(function(e) {
+		var new_filter = 'filter-' + this.value;
+		$('#image figure').removeClass();
+		$('#image figure').addClass(new_filter);
+	});
 });
 
-$('.grid-item').click(function(){
-	var image_data = $(this).data(image);
-	var image = image_data.image;
-	var description = `<p>${image.description}</p>`;
-	var title = `<h5 class="modal-title">${image.name}<i class="fa.fa-times" data-dismiss="modal" aria-label="Close" aria-hidden="true"></i></h5>`;
-	var img = `<img src="${image.upload_location}" alt="${image.name}">`;
-	$('#image-modal .modal-body').html(img + title + description);
-	$('.modal').modal('show');
-});
